7c67cb749d1d89f351060e38296d36cd5dc8d5d5
Taylor H <tttahobbs@gmail.com>
2023-07-24 17:14:15 -0500

    finished a project and started a new one

--- 03-steps/src/App-v1.js
+++ 03-steps/src/App-v1.js
@@ -32,46 +32,26 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
 
           <div className="buttons">
-            <Button
-              textcolor="#fff"
-              backgroundColor="#7950f2"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
             >
-              <span>👈</span> Previous
-            </Button>
-            <Button
-              textcolor="#fff"
-              backgroundColor="#7950f2"
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
             >
-              Next<span>👉</span>
-            </Button>
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}:</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textcolor, backgroundColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: backgroundColor, color: textcolor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
